ef722daeb1b6bb7131e143ac0ece6389569fe9df
Lucas <lucas.peluso@ucsal.edu.br>
2025-08-27 19:35:47 -0300

    m

--- src/main/java/br/com/mariojp/solid/isp/OfficeService.java
+++ src/main/java/br/com/mariojp/solid/isp/OfficeService.java
@@ -1,11 +1,11 @@
 package br.com.mariojp.solid.isp;
 
-public class OfficeService {
+public class OfficeService implements Print{
     private final MultiFunctionDevice device;
     public OfficeService(MultiFunctionDevice device){ this.device = device; }
-    public void printReport(String content){
-        device.print(content);
-        // BUG: chama scan sem necessidade
-        device.scan("audit.txt");
-    }
+	@Override
+	public void printReport(String content) {
+		// TODO Auto-generated method stub
+		device.print(content);
+	}
 }
